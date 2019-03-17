0cdaf75a28a0e746c64b5c2c2615e33860027a42
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-03-16 22:46:43 -0700

    Completed arraySum function

--- src/recursion.js
+++ src/recursion.js
@@ -28,12 +28,22 @@ var sum = function(array) {
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
-    return 
-};
+ var arraySum = (array) => {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'number') { 
+            sum += array[i];
+        }
+        if (array[i] instanceof Array) {
+            sum += arraySum(array[i]); 
+        }
+    }
+    return sum; 
+    }
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+
 };
 
 // 5. Sum all integers below a given integer.
